0f49be37767ee5c05a39fecdf3d4c8d819dc5608
rodsilca <cabralrodrigo787@gmail.com>
2025-10-31 21:25:47 -0400

    feat(middleware): Updated checkIdExists middleware to handle both single-object and array insert requests. Now validates all IDs in the array and returns conflicts if any already exist in the database.

--- backend/middlewares/checkId.js
+++ backend/middlewares/checkId.js
@@ -4,15 +4,33 @@ import Group from "../models/Group.js";
 export function checkIdExists(model){
 
     return async (req,res,next) =>{
-        const id = req.body.Id;
+        const data = req.body;    
+        if(!Array.isArray(data) || data.length===0){
 
-        if(!id) return res.status(400).json({message: "Id is required"});
+            const id = data.Id;
 
-        const exists = await model.findOne({Id: Number(id)});
+            if(!id) return res.status(400).json({message: "Id is required"});
 
-        if(exists) return res.status(409).json({ error: "ID is not available" });
+            const exists = await model.findOne({Id: Number(id)});
+
+            if(exists) return res.status(409).json({ error: "ID is not available" });
+
+            next();
+        }
+
+
+        const ids = data.map(item => item.Id);
+
+        const exists = await model.find({Id:  { $in: ids.map(Number) }});
+
+        if (exists.length > 0) {
+            return res.status(409).json({
+                error: "Some IDs already exist",
+            });
+        }
 
         next();
+
     };
 
 }
